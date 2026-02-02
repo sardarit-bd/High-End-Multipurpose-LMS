989484e3609643f4d3d11a68547eaa75bdd9d518
Md Sohel <sohelf131@gmail.com>
2026-02-02 16:52:37 +0600

    completed order page

--- src/helpers/getDashboardSideMenu.js
+++ src/helpers/getDashboardSideMenu.js
@@ -1,6 +1,6 @@
 import { Home, BookOpen, Users, Settings, ShoppingBag, ClipboardList, BarChart3, Award, User, HelpCircle, HomeIcon, UserCheck, Shield, Trophy, CreditCard, Settings2, Badge, BadgeCent, LocateIcon, ShoppingBasket } from "lucide-react";
 import { FaProductHunt } from "react-icons/fa";
-import { MdCategory } from "react-icons/md";
+import { MdCategory, MdOutlineBorderStyle } from "react-icons/md";
 
 // ===== Menu Configs with Icons ===== //
 const studentMenu = [
@@ -31,6 +31,7 @@ const adminMenu = [
 { title: "Categories", href: "/dashboard/admin/categories", icon: MdCategory },
  { title: "Badges", href: "/dashboard/admin/badges", icon: BadgeCent },
   { title: "Products", href: "/dashboard/admin/products", icon: FaProductHunt },
+  { title: "Orders", href: "/dashboard/admin/orders", icon: MdOutlineBorderStyle },
  { title: "Location", href: "/dashboard/admin/location", icon: LocateIcon},
   { title: "Manage Students", href: "/dashboard/admin/students", icon: Users },
   { title: "Manage Instructors", href: "/dashboard/admin/instructors", icon: UserCheck },
